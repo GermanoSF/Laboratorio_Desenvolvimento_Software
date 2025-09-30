3bb80760fab06d57530a0b0a819b5a369da534ab
GermanoSF <gspallfigueira@gmail.com>
2025-09-30 08:20:15 -0300

    Update pessoa_BD.sql

--- pessoa_BD.sql
+++ pessoa_BD.sql
@@ -7,3 +7,11 @@ CREATE TABLE PESSOA (
      sexo varchar(1) NOT NULL,
      idioma varchar(10) NOT NULL
      );
+
+CREATE TABLE veiculo (
+     id int AUTO_INCREMENT PRIMARY KEY,
+     modelo varchar(50) NOT NULL,
+     placa varchar(1) NOT NULL,
+     id_pessoa int NOT NULL,
+     foreign key(id_pessoa) references pessoa(id)
+     );
